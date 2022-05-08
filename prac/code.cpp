9fa8afaa4653d6ca167a250003db59dc5e0fdf69
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2022-05-08 22:54:23 +0900

    Add files via upload

---
+++ prac/code.cpp
@@ -0,0 +1,9 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+}
+// https://qiita.com/AokabiC/items/e9312856f588dd9303ed (2022/05/07参照)
