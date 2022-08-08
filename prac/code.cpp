ff6b664e65f4602fc1b4206d3c1575cc7cdf4641
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2022-08-08 15:59:59 +0900

    Delete prac directory

--- prac/code.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  cout << "こんにちは" << endl;
-  cout << "AtCoder" << endl;
-}
-// https://qiita.com/AokabiC/items/e9312856f588dd9303ed (2022/05/07参照)
